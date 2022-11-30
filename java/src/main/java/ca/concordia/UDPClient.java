4161581e6c5d63310a2c2d10274afe9c0db99342
cedparadis <84161247+cedparadis@users.noreply.github.com>
2022-11-30 15:59:18 -0500

    added projects (everything seems to work)

---
+++ java/src/main/java/ca/concordia/UDPClient.java
@@ -0,0 +1,332 @@
+package ca.concordia;
+
+import static java.nio.channels.SelectionKey.OP_READ;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.SocketAddress;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class UDPClient {
+
+	private static final Logger logger = LoggerFactory.getLogger(UDPClient.class);
+	private static final int DATA = 0;
+	private static final int SYN = 1;
+	private static final int SYNACK = 2;
+	private static final int ACK = 3;
+	private static final int NACK = 4;
+	private static final int FIN = 5;
+
+	private static HashMap<Long,Packet> ackBuf = new LinkedHashMap<>();
+	
+		/*
+		 * Sending a get method to the server, but save the GET as a string to send as payload
+		 */
+	private static String sendGET(String testUrl, boolean verbose, Map<String,String>headers) {
+		//put in URL to parse elements
+		try {
+			URL url = new URL(testUrl);
+			String host = url.getHost();
+			String path = url.getPath();
+			String param = url.getQuery();
+			int port = url.getPort() != -1 ? url.getPort():url.getDefaultPort();
+			StringBuilder request = new StringBuilder();
+
+			if(param!= null) {
+				request.append( "GET "+ path + "?"+ param + " HTTP/1.0\r\n" );
+			}
+			//without parameters
+			else {
+				request.append( "GET "+ path + " HTTP/1.0\r\n" );
+			}
+
+			request.append("Host: " + host + "\r\n");
+			if(headers != null) {
+				for(String key: headers.keySet()) {
+					request.append(key +": " + headers.get(key) +"\r\n");
+				}
+				request.append("\r\n");
+			}
+			else {
+				request.append("\r\n");
+			}
+			return request.toString();
+			//System.out.println("host: " + host + "\nPath: " + param + "\nPort: " + port);
+		}catch(MalformedURLException e) {
+			return e.getMessage();
+		}
+	}
+
+	/*
+	 * Sending a post method to the server, but save the POST as a string to send as payload
+	 */
+	private static String sendPOST(String testUrl, boolean verbose, Map<String,String> headers, String inlineData) throws IOException{
+		//put in URL to parse elements
+		URL url = new URL(testUrl);
+		String host = url.getHost();
+		String path = url.getPath();
+		String param = url.getQuery();
+		int port = url.getPort() != -1 ? url.getPort():url.getDefaultPort();
+		StringBuilder request = new StringBuilder();
+
+		request.append("POST " + path + " HTTP/1.0\r\n");
+
+		if(headers != null) {
+			for(String key: headers.keySet()) {
+				request.append(key +": " + headers.get(key) + "\r\n");
+			}
+		}
+
+		//set the body of the request
+
+		if(inlineData == null) {
+			request.append("Content-length: 0");
+			request.append("\r\n");
+			request.append("");
+		}
+		else {
+			request.append("Content-length: " + inlineData.length() + "\r\n");
+			request.append("\r\n");
+
+			request.append(inlineData);
+
+		}
+		return request.toString();
+	}
+
+
+
+
+	private static void runClient(SocketAddress routerAddr, InetSocketAddress serverAddr) throws IOException {
+		try(DatagramChannel channel = DatagramChannel.open()){
+			long sequenceNumber = handshake(channel,routerAddr, serverAddr);
+			if(sequenceNumber == -1) {
+				logger.error("No response after timeout");
+				return;
+			}
+			String msg = sendGET("http://localhost/", true, null);
+			Packet p = new Packet.Builder()
+					.setType(DATA)
+					.setSequenceNumber(1L)
+					.setPortNumber(serverAddr.getPort())
+					.setPeerAddress(serverAddr.getAddress())
+					.setPayload(msg.getBytes())
+					.create();
+			channel.send(p.toBuffer(), routerAddr);
+
+			logger.info("Sending \"{}\" to router at {}", msg, routerAddr);
+
+			// Try to receive a packet within timeout. Can receive multiple packets
+			while(true) {
+				//check if delay too long, resend packet
+				timer(channel,p,routerAddr);
+
+				// We just want a single response.
+				ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
+				SocketAddress router = channel.receive(buf);
+				buf.flip();
+				Packet resp = Packet.fromBuffer(buf);
+				//check if after sending data we do not get an ACK from server, we send again
+				if(!(resp.getType() == ACK && resp.getSequenceNumber() == p.getSequenceNumber())){
+					channel.send(p.toBuffer(), routerAddr);
+					continue;
+				}
+				logger.info("Packet: {}", resp);
+				logger.info("Router: {}", router);
+				String payload = new String(resp.getPayload(), StandardCharsets.UTF_8);
+				logger.info("Payload: {}",  payload);
+
+				//build a FIN packet to tell server we want to close connection after its data
+				Packet f = resp.toBuilder()
+						.setType(FIN)
+						.setSequenceNumber(resp.getSequenceNumber()+1)
+						.setPortNumber(serverAddr.getPort())
+						.setPeerAddress(serverAddr.getAddress())
+						.setPayload("FIN".getBytes())
+						.create();
+				channel.send(f.toBuffer(), routerAddr);
+
+				logger.info("Sending \"{}\" to router at {}", "FIN", routerAddr);
+
+				// method to handle multiple packets received from server
+				receiveData(channel, routerAddr, serverAddr, f);
+
+				break;
+
+
+			} 
+		}
+	}
+
+			/*
+			 * Method to handle the data from server and send back ACK for each data to ensure it is all received
+			 */
+	private static void receiveData(DatagramChannel channel, SocketAddress routerAddr, InetSocketAddress serverAddr, Packet packet) throws IOException{
+		StringBuilder totalPayload = new StringBuilder(); //place all data packet in one string
+		while(true) {
+			timer(channel,packet,routerAddr); //check if delay too long send back FIN
+
+			// We just want a single response.
+			ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
+			SocketAddress router = channel.receive(buf);
+			buf.flip();
+			Packet resp = Packet.fromBuffer(buf);
+
+			//append the data from the packet
+			if(resp.getType()==DATA) {
+				logger.info("Data packet received from Server");
+				totalPayload.append(new String(resp.getPayload(),StandardCharsets.UTF_8));
+			}
+			//send an ack packet to notify the server we received 
+			//TODO: method to send back ACK if it was dropped (create timer method in server for dropped ACK)
+			Packet respAck = resp.toBuilder()
+					.setType(ACK)
+					.setSequenceNumber(resp.getSequenceNumber())
+					.setPortNumber(serverAddr.getPort())
+					.setPeerAddress(serverAddr.getAddress())
+					.setPayload("ACK".getBytes())
+					.create();
+
+			channel.send(respAck.toBuffer(), router);
+			/* if(!(resp.getType() == ACK && resp.getSequenceNumber() == p.getSequenceNumber())){
+             	channel.send(p.toBuffer(), routerAddr);
+             	continue;
+             }*/
+			
+			//If server sends FYN it means there are no more data, end the connection and display payload
+			
+			if(resp.getType()==FIN) {
+				logger.info("FYN received from Server");
+				logger.info("Connection is now over");
+				System.out.println(totalPayload.toString());
+				return;
+			}
+		}
+	}
+
+
+				/*
+				 * Method to initiate handshake with server
+				 */
+	private static long handshake(DatagramChannel channel,SocketAddress routerAddr, InetSocketAddress serverAddr) throws IOException {
+		String payload = "SYN";
+		Packet syn = new Packet.Builder()
+				.setType(SYN)
+				.setSequenceNumber(1L)
+				.setPortNumber(serverAddr.getPort())
+				.setPeerAddress(serverAddr.getAddress())
+				.setPayload(payload.getBytes())
+				.create();
+		//Send a SYN to initiate handshake
+		logger.info("Initiating connection, sending Handshake SYN");
+		channel.send(syn.toBuffer(), routerAddr);
+
+		//Try to receive SYNACK from server
+		timer(channel,syn,routerAddr);
+
+		ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
+		channel.receive(buf);
+		buf.flip();
+		Packet resp = Packet.fromBuffer(buf);
+
+		// String msg = new String(resp.getPayload(), StandardCharsets.UTF_8);
+
+
+		//check if it received SYN-ACK
+		if(resp.getType() == SYNACK) {
+			logger.info("Received SYNACK Handshake");
+			payload = "ACK";
+			//send ACK
+			Packet ack = new Packet.Builder()
+					.setType(ACK)
+					.setSequenceNumber(3L)
+					.setPortNumber(serverAddr.getPort())
+					.setPeerAddress(serverAddr.getAddress())
+					.setPayload(payload.getBytes())
+					.create();
+			logger.info("Sending Handshake ACK, connection established.");
+			channel.send(ack.toBuffer(), routerAddr);	
+			return ack.getSequenceNumber();
+
+		}
+		else {
+			return -1;
+		}
+
+
+
+
+	}
+
+					/*
+					 * Method to resend packet after delay
+					 * TODO:implement same timer method in SERVER
+					 */
+	private static void timer(DatagramChannel channel, Packet packet, SocketAddress router )throws IOException{
+		channel.configureBlocking(false);
+		Selector selector = Selector.open();
+		channel.register(selector, OP_READ);
+		selector.select(1000);
+
+		Set<SelectionKey>keys = selector.selectedKeys();
+		if(keys.isEmpty()) {
+			channel.send(packet.toBuffer(), router);
+			timer(channel,packet,router);
+		}
+		keys.clear();
+		return;
+	}
+
+
+
+	public static void main(String[] args) throws IOException {
+		OptionParser parser = new OptionParser();
+		parser.accepts("router-host", "Router hostname")
+		.withOptionalArg()
+		.defaultsTo("localhost");
+
+		parser.accepts("router-port", "Router port number")
+		.withOptionalArg()
+		.defaultsTo("3000");
+
+		parser.accepts("server-host", "EchoServer hostname")
+		.withOptionalArg()
+		.defaultsTo("localhost");
+
+		parser.accepts("server-port", "EchoServer listening port")
+		.withOptionalArg()
+		.defaultsTo("8007");
+
+		OptionSet opts = parser.parse(args);
+
+		// Router address
+		String routerHost = (String) opts.valueOf("router-host");
+		int routerPort = Integer.parseInt((String) opts.valueOf("router-port"));
+
+		// Server address
+		String serverHost = (String) opts.valueOf("server-host");
+		int serverPort = Integer.parseInt((String) opts.valueOf("server-port"));
+
+		SocketAddress routerAddress = new InetSocketAddress(routerHost, routerPort);
+		InetSocketAddress serverAddress = new InetSocketAddress(serverHost, serverPort);
+
+		runClient(routerAddress, serverAddress);
+	}
+}
+
---
+++ java/src/main/java/ca/concordia/UDPServer.java
@@ -0,0 +1,380 @@
+package ca.concordia;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class UDPServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(UDPServer.class);
+    private String directory = "C:/Users/Cedric Paradis/Documents/";
+    private static final int DATA = 0;
+    private static final int SYN = 1;
+    private static final int SYNACK = 2;
+    private static final int ACK = 3;
+    private static final int NACK = 4;
+    private static final int FIN = 5;
+    
+    HashMap<Long,String> finalResponse = new LinkedHashMap<>();
+    
+   
+    		/*
+    		 * Return list of files in directory OR content of a specific file
+    		 * Saved in a string to put in packet payload
+    		 */
+    private String handleGET(String file) {
+    	if(file.equals("/")) {
+     		File dir = new File(directory);
+ 			File[]list = dir.listFiles();
+ 			StringBuilder listing = new StringBuilder();
+ 			StringBuilder directoryBuilder = new StringBuilder("========\n");
+ 			int i = 0;
+ 			while(i < list.length) {
+ 				if(list[i].isFile()) {
+ 					directoryBuilder.append("File: ");
+ 				}
+ 				else if(list[i].isDirectory()) {
+ 					directoryBuilder.append("Directory: ");
+ 				}
+ 				else
+ 					continue;
+ 				directoryBuilder.append(list[i].getName() + "\n");
+ 				i++;
+ 			}
+ 			listing.append("HTTP/1.0 200 Ok\n");
+ 			listing.append("Date: " + java.time.LocalDate.now() +"\n");
+ 			listing.append("Server: localhost\n");
+ 			listing.append("\n\n");
+ 			listing.append(directoryBuilder.toString());
+ 			return listing.toString();
+    	}
+    	else {
+    		try {
+
+				File f = new File(directory + file);
+				File t = new File(file);
+				StringBuilder listing = new StringBuilder();
+				if(!f.exists()) {
+					listing.append("HTTP/1.0 405 Not Found\r\n");
+					listing.append("Date: " + java.time.LocalDate.now()+"\r\n");
+					listing.append(f);
+					return listing.toString();
+				}
+				else if(unAuthorized(file)) {
+					listing.append("HTTP/1.0 403 Forbidden");
+					listing.append("Date: " + java.time.LocalDate.now());
+					return listing.toString();
+				}
+				else if(f.isFile()) {
+					BufferedReader reader = new BufferedReader(new FileReader(f));
+					StringBuilder content = new StringBuilder("Content: \n");
+					String line = reader.readLine();
+					while(line!=null) {
+						content.append(line);
+						content.append("\n");
+						line = reader.readLine();
+					}
+					reader.close();
+					listing.append("HTTP/1.0 200 Ok\n");
+					listing.append("Date: " + java.time.LocalDate.now() +"\n");
+					listing.append("Server: localhost\n");
+					listing.append("\n\n");
+					listing.append(content.toString());
+					return listing.toString();
+				}
+
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				return e.getMessage();
+			}
+			catch(IOException i) {
+				return i.getMessage();
+			}
+    	}
+		return "";
+    }
+    /*
+	 * Create content in a file
+	 * Saved in a string to put in packet payload
+	 */
+    private String handlePOST(String file, String body) {
+    	PrintWriter fileWriter;
+    	StringBuilder response = new StringBuilder();
+    	if(unAuthorized(file)) {
+			response.append("HTTP/1.0 403 Forbidden\r\n");
+			response.append("Date: " + java.time.LocalDate.now() + "\r\n");
+			return response.toString();
+		}
+    	else {
+    		try {
+    			fileWriter = new PrintWriter(new FileOutputStream(directory+file, false));
+				fileWriter.println(body);
+				fileWriter.flush();
+				fileWriter.close();
+    		response.append("HTTP/1.0 201 Created\r\n");
+    		response.append("Date: " + java.time.LocalDate.now() + "\r\n");
+    		response.append("Server: localhost\r\n");
+    		response.append("Content-length: " + file.length() + "\r\n");
+    		response.append("\r\n");
+    		return response.toString();
+    		}catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch(IOException i) {
+				System.out.println(i);
+			}
+    	}
+		return "";
+    }
+    				/*
+    				 * Method to extract payload from client packet
+    				 * Create response packet payload 
+    				 */
+    private String createResponse(String directory, String payload) {
+    	String[] requestArray1 = payload.split("\r\n\r\n");
+    	String[] requestArray2 = requestArray1[0].split("\r\n");
+    	String[] separator = requestArray2[0].split(" ");
+    	String method = separator[0];
+    	String file = separator[1].replace("HTTP/1.0","");
+        
+        if(file.length() > 1) {
+			file = file.startsWith("/") ? file.substring(1) : file;
+		}
+    	 if(method.equals("GET")) {
+    		 
+         	return handleGET(file);
+         	
+         }else if(method.equals("POST")) {
+        	return handlePOST(file, requestArray1[1]);
+         }
+    	 return "";
+    	
+    }
+    
+    
+    private boolean unAuthorized(String file) {
+		File f = new File(file);
+		File currentDir = new File(directory);
+
+		if(f.isDirectory()) {
+			return true;
+		}
+		//check if file is outside current dir
+		try {
+			if(f.getCanonicalPath().contains(currentDir.getCanonicalPath())) {
+				return false; // able to write here
+			}
+		} catch(IOException e) {
+			return true;
+		}
+
+		return false;
+
+	}
+    
+    						/*
+    						 * Method to handle a response that is bigger than packet size
+    						 * Put the response in multiple packets and stored in Hashmap
+    						 */
+    private HashMap<Long,Packet> handleResponse(String response, Packet packet, SocketAddress router, DatagramChannel channel){
+    	HashMap<Long,Packet> responseMap = new LinkedHashMap<>(); //linkedHashMap keep insertion order
+    	long sequenceNumber = packet.getSequenceNumber() +1;
+    	byte[] payload = response.getBytes();
+    	
+    	if(payload.length <= Packet.MAX_PAYLOAD) {
+    		Packet resp = packet.toBuilder().setSequenceNumber(sequenceNumber).setType(DATA).setPayload(payload).create();
+    		responseMap.put(resp.getSequenceNumber(), resp);
+    		sequenceNumber = sequenceNumber + resp.getPayload().length + Packet.MIN_LEN;
+    	}
+    	else {
+    		int currentIndex = 0;
+    		while(currentIndex < payload.length) {
+    			int maxIndex = Math.min(currentIndex + Packet.MAX_PAYLOAD, payload.length);
+    			byte[] packetPayload = new byte[maxIndex - currentIndex];
+    			int i = 0;
+    			for(int j = currentIndex; j < maxIndex;j++) {
+    				packetPayload[i] = payload[j]; 
+    				i++;
+    			}
+    			sequenceNumber = sequenceNumber + currentIndex;
+    			Packet resp = packet.toBuilder().setSequenceNumber(sequenceNumber).setType(DATA).setPayload(packetPayload).create();
+    			responseMap.put(sequenceNumber, resp);
+    			currentIndex = maxIndex;
+    		}
+    	}
+    	//putting a FIN at the end of the hashmap of data packets
+    	Packet fin = packet.toBuilder().setSequenceNumber(sequenceNumber+1).setType(FIN).setPayload("END of connection".getBytes()).create();
+    	responseMap.put(fin.getSequenceNumber(), fin);
+    	//logger.info("Sending FIN packet to the server");
+    	return responseMap;
+    }
+    
+    				/*
+    				 * Send the packets and check if we have ACK from client before sending it back
+    				 */
+    private void sendPackets(HashMap<Long,Packet> response, DatagramChannel channel, SocketAddress router, ByteBuffer buffer) throws IOException{
+    	for(Map.Entry<Long, Packet>packet: response.entrySet()) {
+    		if(packet.getValue().getType() == DATA) {
+    		channel.send(packet.getValue().toBuffer(),router );
+    		checkAck(channel, router, packet.getValue(), buffer);
+    		}
+    		else {
+    			channel.send(packet.getValue().toBuffer(), router);
+    			
+    		}
+    	}
+    	
+    }
+    
+    private void checkAck(DatagramChannel channel, SocketAddress router, Packet packet, ByteBuffer buffer) throws IOException{
+    	Packet rcv = null;
+    	buffer.clear();
+    	router = channel.receive(buffer);
+    	buffer.flip();
+        rcv = Packet.fromBuffer(buffer);
+        buffer.flip();
+        if(!(rcv.getType()==ACK && packet.getSequenceNumber() == rcv.getSequenceNumber())) {
+        	channel.send(packet.toBuffer(), router);
+        	checkAck(channel,router,packet, buffer);
+        }
+    }
+    
+    
+    private void listenAndServe(int port) throws IOException {
+
+        try (DatagramChannel channel = DatagramChannel.open()) {
+            channel.bind(new InetSocketAddress(port));
+            logger.info("EchoServer is listening at {}", channel.getLocalAddress());
+            ByteBuffer buf = ByteBuffer
+                    .allocate(Packet.MAX_LEN)
+                    .order(ByteOrder.BIG_ENDIAN);
+
+            for (; ; ) {
+                buf.clear();
+                SocketAddress router = channel.receive(buf);
+
+                // Parse a packet from the received raw data.
+               
+
+             
+                
+        
+                
+                
+                
+                HashMap<Long,Packet> routerResponse = choosePacket(buf,directory,router,channel);
+                if(routerResponse != null) {
+                	sendPackets(routerResponse,channel,router, buf);
+                }
+                
+                
+                // Send the response to the router not the client.
+                // The peer address of the packet is the address of the client already.
+                // We can use toBuilder to copy properties of the current packet.
+                // This demonstrate how to create a new packet from an existing packet.
+               /* Packet resp = packet.toBuilder()
+                        .setPayload(response.getBytes())
+                        .create();
+                channel.send(resp.toBuffer(), router);
+*/
+            }
+        }
+    }
+    
+    private HashMap<Long,Packet> choosePacket(ByteBuffer buffer, String directory, SocketAddress router, DatagramChannel channel) throws IOException{
+    	Packet packet = null;
+    	HashMap<Long, Packet> packets = new HashMap<>();
+    	buffer.flip();
+        packet = Packet.fromBuffer(buffer);
+        buffer.flip();
+        String payload = new String(packet.getPayload(), UTF_8);
+        if(packet.getType() == SYN) {
+        	return handleHandshake(packet);
+        }
+        else if(packet.getType() == ACK) {
+        	logger.info("ACK received");
+        	return null;
+        }
+        else if(packet.getType()== DATA) {
+        	logger.info("DATA packet received");
+        	finalResponse.put(packet.getSequenceNumber(), payload);
+        	logger.info("Sending ACK for data received");
+        	return sendAck(packet);
+        }
+        else if(packet.getType()== FIN) {
+        	logger.info("FIN packet received");
+        	StringBuilder messageBuilder = new StringBuilder();
+        	SortedSet<Long> keys = new TreeSet<>(finalResponse.keySet());
+        	for(long key : keys) {
+        		messageBuilder.append(finalResponse.get(key));
+        	}
+        	logger.info("Sending DATA packet");
+        	String response = createResponse(directory,messageBuilder.toString());
+        	return handleResponse(response,packet,router,channel);
+        }
+        else {
+        	logger.info("Invalid type");
+        	return null;
+        }
+    }
+
+    private HashMap<Long,Packet> handleHandshake(Packet packet){
+    	HashMap<Long,Packet> handshakeResponse= new HashMap<>();
+    	String message = "SYNACK";
+    	Packet response = packet.toBuilder()
+    			.setSequenceNumber(packet.getSequenceNumber()+1)
+    			.setType(SYNACK)
+    			.setPayload(message.getBytes(StandardCharsets.UTF_8))
+    			.create();
+    	handshakeResponse.put(packet.getSequenceNumber(), response);
+    	logger.info("Handshake SYN received");
+    	return handshakeResponse;
+    }
+    
+    
+    private HashMap<Long,Packet> sendAck(Packet packet)throws IOException{
+    	HashMap<Long,Packet> responseMap = new HashMap<>();
+    	Packet response = packet.toBuilder().setSequenceNumber(packet.getSequenceNumber()).setPayload("ACK".getBytes()).setType(ACK).create();
+    	responseMap.put(packet.getSequenceNumber(),response);
+    	return responseMap;
+    }
+    
+    
+    
+    public static void main(String[] args) throws IOException {
+        OptionParser parser = new OptionParser();
+        parser.acceptsAll(asList("port", "p"), "Listening port")
+                .withOptionalArg()
+                .defaultsTo("8007");
+
+        OptionSet opts = parser.parse(args);
+        int port = Integer.parseInt((String) opts.valueOf("port"));
+        UDPServer server = new UDPServer();
+        server.listenAndServe(port);
+    }
+}
